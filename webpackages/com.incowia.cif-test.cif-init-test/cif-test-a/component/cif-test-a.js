13c24c777afddbd69f828affd15c2c57199db17a
Judit Ross <jtrs@incowia.de>
2015-11-10 16:23:46 +0100

    PLAT-323: Cif-test projekt

---
+++ webpackages/com.incowia.cif-test.cif-init-test/cif-test-a/component/cif-test-a.js
@@ -0,0 +1,38 @@
+(function() {
+    'use strict';
+    CubxPolymer({
+        is: 'cif-test-a',
+        ready: function() {
+            this.$.slota.setAttribute('value', this.getA());
+            this.$.slotb.setAttribute('value', this.getB());
+            this.$.slotc.setAttribute('value', this.getC());
+            this.$.slotd.setAttribute('value', this.getD());
+        },
+        changeAHandler: function(event) {
+            this.setA(event.target.value);
+            this.set('model.a', event.target.value);
+        },
+        changeBHandler: function(event) {
+            this.setB(event.target.value);
+        },
+        changeCHandler: function(event) {
+            this.setC(event.target.value);
+        },
+        changeDHandler: function(event) {
+            this.setD(event.target.value);
+        },
+        modelAChanged: function() {
+            this.$.slota.setAttribute('value', this.getA());
+        },
+        modelBChanged: function() {
+            this.$.slotb.setAttribute('value', this.getB());
+        },
+        modelCChanged: function() {
+            this.$.slotc.setAttribute('value', this.getC());
+        },
+        modelDChanged: function() {
+            this.$.slotd.setAttribute('value', this.getD());
+        }
+    });
+
+}());
