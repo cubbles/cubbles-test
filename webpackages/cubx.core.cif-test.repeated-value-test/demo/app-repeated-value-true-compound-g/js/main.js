eaa953a8e2e6433e8f624705fea4b16b6c4688cd
Judit Ross <jtrs@incowia.de>
2015-12-07 08:52:30 +0100

    PLAT-280: scenario für repeatedValue-true + bootstrap grid

---
+++ webpackages/cubx.core.cif-test.repeated-value-test/demo/app-repeated-value-true-compound-g/js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+(function() {
+
+    document.addEventListener('cifReady', function() {
+        document.querySelector('[cubx-core-crc]').classList.add('container-fluid');
+        document.querySelector('repeated-value-true-compound-g').classList.add('row');
+        document.querySelector('repeated-value-elementary-e').classList.add('col-md-6');
+        document.querySelector('repeated-value-elementary-f').classList.add('col-md-6');
+    });
+})();
---
+++ webpackages/cubx.core.cif-test.repeated-value-test/repeated-value-elementary-e/repeated-value-elementary-e.js
@@ -0,0 +1,31 @@
+(function() {
+    'use strict';
+    /**
+     * Get help:
+     * > Lifecycle callbacks:
+     * https://www.polymer-project.org/1.0/docs/devguide/registering-elements.html#lifecycle-callbacks
+     *
+     * Access the Cubixx-Component-Model:
+     * > Access slot values:
+     * slot 'a': this.getA(); | this.setA(value)
+     */
+    CubxPolymer({
+        is: 'repeated-value-elementary-e',
+
+        /**
+         * Manipulate an element’s local DOM when the element is constructed.
+         */
+        ready: function() {
+        },
+
+
+        /**
+         * A handler to be called by a dom-element
+         * @param {event} event
+         */
+        clickHandler: function(event) {
+            // update the cubixx-model
+            this.setClicked(true);
+        }
+    });
+}());
