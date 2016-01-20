0906b701ab7bbd19d52dbd8f81f31dc47771f518
Judit Ross <jtrs@incowia.de>
2016-01-20 12:50:01 +0100

    PLAT-335: Demoscenario addConnection per DragAndDrop

--- webpackages/cubx.core.rte-test.dynamic-connection/container-element/container-element.js
+++ webpackages/cubx.core.rte-test.dynamic-connection/container-element/container-element.js
@@ -1,3 +1,4 @@
+/* globals findAncestorElement,elementFindByAttributeValue  */
 (function() {
     'use strict';
     /**
@@ -16,7 +17,11 @@
          * Manipulate an element’s local DOM when the element is constructed.
          */
         ready: function() {
-            // set value-attribute of element with id='slota' to the initial value of slot 'a'
+            this.$.dropzone.addEventListener('dragenter', this.handleDragEnter);
+            this.$.dropzone.addEventListener('dragover', this.handleDragOver);
+            this.$.dropzone.addEventListener('dragleave', this.handleDragLeave);
+            this.$.dropzone.addEventListener('dragend', this.handleDragEnd);
+            this.$.dropzone.addEventListener('drop', this.handleDrop);
         },
 
         /**
@@ -24,6 +29,54 @@
          */
         modelAChanged: function(newValue) {
 
+        },
+        handleDragEnter: function(e) {
+            //console.log('dragenter', e.target);
+            this.classList.add('over');
+        },
+        handleDragOver: function(e) {
+            //console.log('dragover', e.targer);
+            if (e.preventDefault) {
+                e.preventDefault(); // Necessary. Allows us to drop.
+            }
+            e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+            return false;
+        },
+        handleDragLeave: function(e) {
+            //console.log('dragleave');
+            this.classList.remove('over');  // this / e.target is previous target element.
+        },
+        handleDragEnd: function(e) {
+            //console.log('dragend');
+            this.classList.remove('over');  // this / e.target is previous target element.
+        },
+        handleDrop: function(e) {
+            if (e.stopPropagation) {
+                e.stopPropagation(); // stops the browser from redirecting.
+                var runtimeId = e.dataTransfer.getData('runtimeId');
+                var me = e.target;
+                var host = findAncestorElement(me, 'container-element');
+                if (!host) {
+                    throw new Error('parent "container-element" not found.');
+                }
+                //console.log('container-element:drop:host', host);
+                var draggedEl = elementFindByAttributeValue('runtime-id', runtimeId);
+                me.appendChild(draggedEl);
+                host.createConnectionTo(draggedEl);
+            }
+            return false;
+        },
+
+        createConnectionTo: function(draggedElement){
+            var dynamicConnection = new window.cubx.cif.DynamicConnection();
+            dynamicConnection.setSourceRuntimeId(this.getAttribute('runtime-id'));
+            dynamicConnection.setSourceSlot('message');
+            dynamicConnection.setDestinationRuntimeId(draggedElement.getAttribute('runtime-id'));
+            dynamicConnection.setDestinationSlot('message');
+            dynamicConnection.setConnectionId('autoconnected')
+            this.addConnection(dynamicConnection);
+
         }
+
     });
 }());
--- webpackages/cubx.core.rte-test.dynamic-connection/transportable-element/transportable-element.js
+++ webpackages/cubx.core.rte-test.dynamic-connection/transportable-element/transportable-element.js
@@ -1,3 +1,4 @@
+/* globals findAncestorElement */
 (function() {
     'use strict';
     /**
@@ -16,10 +17,28 @@
          * Manipulate an element’s local DOM when the element is constructed.
          */
         ready: function() {
+            this.addEventListener('dragstart', this.handleDragStart);
+            this.addEventListener('dragend', this.handleDragEnd);
+
             // set value-attribute of element with id='slota' to the initial value of slot 'a'
+        },
+        handleDragStart: function(e) {
+            //console.log('dragstart');
+            this.style.opacity = '0.4';
+            var me = e.target;
+            var host = findAncestorElement(me,'transportable-element');
+            if (!host) {
+                throw new Error('Parent "transportable-element" not found.');
+            }
+            console.log('transportable-element:dragstart:host', host);
+            var runtimeId = host.getAttribute('runtime-id');
+            e.dataTransfer.setData('runtimeId', runtimeId);
+            e.dataTransfer.effectAllowed = 'move';
+        },
+        handleDragEnd: function(e) {
+            //console.log('dragend');
+            this.style.opacity = '1';
         }
-
-
 
     });
 }());
