8e7fae7d2a9b49bd9948f5a145ff93eb6267c74f
Judit Ross <jtrs@incowia.com>
2018-02-15 08:54:18 +0100

    RTE-125 add elementary eith template

---
+++ webpackages/cubx.core.rte.cubxcomponent.component-test/elementary-with-template/elementary-with-template.js
@@ -0,0 +1,56 @@
+(function () {
+  'use strict';
+  /**
+   * Get help:
+   * > Lifecycle callbacks:
+   * https://www.polymer-project.org/1.0/docs/devguide/registering-elements.html#lifecycle-callbacks
+   *
+   */
+  CubxComponent({
+    is: 'elementary-with-template',
+
+    /**
+     * Manipulate an element’s local DOM when the element is created.
+     */
+    created: function () {
+      this.addMessage('#created called');
+    },
+
+    /**
+     * Manipulate an element’s local DOM when the element is created and initialized.
+     */
+    ready: function () {
+      this.addMessage('#ready called');
+      var label = document.querySelector('[for = input]');
+      label.innerHTML = this.getLabel();
+      var element = document.querySelector('#input');
+      element.value = this.getValue() ? this.getValue() : '';
+      element.addEventListener('change', this.inputChangeHandler.bind(this));
+    },
+
+    inputChangeHandler: function (event) {
+      this.setValue(event.target.value);
+    },
+    /**
+     * Manipulate an element’s local DOM when the element is attached to the document.
+     */
+    connected: function () {
+      this.addMessage('#connected called');
+    },
+
+    /**
+     * Manipulate an element’s local DOM when the cubbles framework is initialized and ready to work.
+     */
+    cubxReady: function () {
+      this.addMessage('#cubxReady called');
+    },
+
+    modelValueChanged: function (value) {
+      this.addMessage('#modelValueChanged called with "' + value + '"');
+    },
+
+    addMessage: function (message) {
+      this.setMessage(this.getMessage() ? this.getMessage() + '<br>' + message : message);
+    }
+  });
+}());
