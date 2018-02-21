dc35c4628a648f8522de90174d7ab96a3abdc1ba
Judit Ross <jtrs@incowia.com>
2018-02-21 12:42:51 +0100

    RTE-137 Update webpackage cubx.core.rte.cif-test.cif-init-encode-test to RTE version 3.0.0

--- webpackages/cubx.core.rte.cif-test.cif-init-encode-test/text-output/text-output.js
+++ webpackages/cubx.core.rte.cif-test.cif-init-encode-test/text-output/text-output.js
@@ -1,41 +1,12 @@
 (function () {
   'use strict';
-  /**
-   * Get help:
-   * > Lifecycle callbacks:
-   * https://www.polymer-project.org/1.0/docs/devguide/registering-elements.html#lifecycle-callbacks
-   *
-   */
-  CubxPolymer({
+
+  CubxComponent({
     is: 'text-output',
-
-    /**
-     * Manipulate an element’s local DOM when the element is created.
-     */
-    created: function () {
-    },
-
-    /**
-     * Manipulate an element’s local DOM when the element is created and initialized.
-     */
-    ready: function () {
-    },
-
-    /**
-     * Manipulate an element’s local DOM when the element is attached to the document.
-     */
-    attached: function () {
-    },
-
-    /**
-     * Manipulate an element’s local DOM when the cubbles framework is initialized and ready to work.
-     */
-    cubxReady: function () {
-    },
 
     modelTextChanged: function (value) {
       console.log(value);
-      var content = this.$$('.content');
+      var content = this.querySelector('.content');
       if (typeof value === 'string') {
         content.innerHTML = value.replace(/\\"/g, '"');
       } else if (typeof value === 'object') {
