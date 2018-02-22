0a0cda4d261dc0f8df7d3c76e883c78765dc732b
Judit Ross <jtrs@incowia.com>
2018-02-22 10:51:50 +0100

    RTE-137 Update webpackage cubx.core.rte.cif-test.connectionhook-scenario to RTE version 3.0.0

--- webpackages/cubx.core.rte.cif-test.connectionhook-scenario/message-viewer/message-viewer.js
+++ webpackages/cubx.core.rte.cif-test.connectionhook-scenario/message-viewer/message-viewer.js
@@ -1,31 +1,22 @@
 (function () {
   'use strict';
-  /**
-   * Get help:
-   * > Lifecycle callbacks:
-   * https://www.polymer-project.org/1.0/docs/devguide/registering-elements.html#lifecycle-callbacks
-   *
-   * Access the Cubbles-Component-Model:
-   * > Access slot values:
-   * slot 'a': this.getA(); | this.setA(value)
-   */
-  CubxPolymer({
+  CubxComponent({
     is: 'message-viewer',
 
-    /**
-     * Manipulate an element’s local DOM when the element is constructed.
-     */
     ready: function () {
-
+      if (this.getMessage()) {
+        this.querySelector('#message').value = this.getMessage();
+      }
+      this.querySelector('#message').addEventListener('input', this.inputHandler.bind(this));
     },
 
-    cubxReady: function () {
-      // this.$.runtimeid.innerHTML = this.getAttribute('runtime-id');
+    inputHandler: function (evt) {
+      this.setMessage(evt.target.value);
     },
-    /**
-     *  Observe the Cubbles-Component-Model: If value for slot 'a' has changed ...
-     */
+
+
     modelMessageChanged: function (newValue) {
+      this.querySelector('#message').value = newValue
     }
   });
 }());
