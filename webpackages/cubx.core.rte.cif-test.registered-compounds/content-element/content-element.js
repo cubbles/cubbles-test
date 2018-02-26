27a6eefa6434484e4715a60dd9c5ba19b7547b77
Judit Ross <jtrs@incowia.com>
2018-02-26 16:37:04 +0100

    RTE-137 Update webpackage cubx.core.rte.cif-test.registered-compounds to RTE version 3.0.0

--- webpackages/cubx.core.rte.cif-test.registered-compounds/content-element/content-element.js
+++ webpackages/cubx.core.rte.cif-test.registered-compounds/content-element/content-element.js
@@ -1,26 +1,14 @@
 (function () {
   'use strict';
-  /**
-   * Get help:
-   * > Lifecycle callbacks:
-   * https://www.polymer-project.org/1.0/docs/devguide/registering-elements.html#lifecycle-callbacks
-   *
-   * Access the Cubbles-Component-Model:
-   * > Access slot values:
-   * slot 'a': this.getA(); | this.setA(value)
-   */
-  CubxPolymer({
+  CubxComponent({
     is: 'content-element',
 
-    /**
-     * Manipulate an element’s local DOM when the element is constructed.
-     */
     ready: function () {
 
     },
     modelMessageChanged: function (newValue) {
       console.log('modelMessageChanged ', newValue);
-      this.$.content.innerHTML = newValue;
+      this.querySelector('#content').innerHTML = newValue;
     }
   });
 }());
