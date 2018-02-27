20571eb727be6778b0acd8ede92ed641b53f5dfa
Judit Ross <jtrs@incowia.com>
2018-02-27 12:41:16 +0100

    RTE-137 Update webpackage cubx.core.rte.crc-test.mutation-based-cubx-startevent-test to RTE version 3.0.0

--- webpackages/cubx.core.rte.crc-test.mutation-based-cubx-startevent-test/test-textarea/test-textarea.js
+++ webpackages/cubx.core.rte.crc-test.mutation-based-cubx-startevent-test/test-textarea/test-textarea.js
@@ -1,39 +1,48 @@
 (function () {
   'use strict';
-  /**
-   * Get help:
-   * > Lifecycle callbacks:
-   * https://www.polymer-project.org/1.0/docs/devguide/registering-elements.html#lifecycle-callbacks
-   *
-   * Access the Cubbles-Component-Model:
-   * > Access slot values:
-   * slot 'a': this.getA(); | this.setA(value)
-   */
-  CubxPolymer({
+  CubxComponent({
     is: 'test-textarea',
 
-    /**
-     * Manipulate an element’s local DOM when the element is created.
-     */
-    created: function () {
-    },
-
-    /**
-     * Manipulate an element’s local DOM when the element is created and initialized.
-     */
     ready: function () {
+      this.querySelector('label').setAttribute('for', this.getId());
+      this.querySelector('textarea').id = this.getId();
+      if (this.getValue()) {
+        this.querySelector('textarea').value = this.getValue();
+      }
+      if (this.getLabel()) {
+        this.querySelector('label').innerHTML = this.getLabel();
+      }
+      if (this.getCols()) {
+        this.querySelector('textarea').setAttribute('cols', this.getCols());
+      }
+      if (this.getRows()) {
+        this.querySelector('textarea').setAttribute('rows', this.getRows());
+      }
     },
 
-    /**
-     * Manipulate an element’s local DOM when the element is attached to the document.
-     */
-    attached: function () {
+    modelIdChanged: function (value) {
+      this.querySelector('label').setAttribute('for', value);
+      this.querySelector('textarea').id = value;
     },
 
-    /**
-     * Manipulate an element’s local DOM when the cubbles framework is initialized and ready to work.
-     */
-    cubxReady: function () {
+    modelValueChanged: function (value) {
+      if (!value) {
+        value = '';
+      }
+      this.querySelector('textarea').value = value;
+    },
+
+    modelLabelChanged: function (value) {
+      if (!value) {
+        value = '';
+      }
+      this.querySelector('label').innerHTML = value;
+    },
+    modelColsChanged: function (value) {
+      this.querySelector('textarea').setAttribute('cols', value);
+    },
+    modelRowsChanged: function (value) {
+      this.querySelector('textarea').setAttribute('rows', value);
     }
   });
 }());
